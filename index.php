bcb4bb897fa6e08893016f505ef07998c2365659
Kaspar Martin Suursalu <kasparsu@gmail.com>
2022-02-11 19:34:50 +0200

    add basic variables and output

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+$foo = 1;
+
+echo "hello\t world\n";
+echo $foo;
+var_dump($foo);
+$foo = "hello world";
+var_dump($foo);
+$foo = 1.2;
+var_dump($foo);
+$foo = true;
+var_dump($foo);
+$foo = false;
+var_dump($foo);
+$foo = null;
+var_dump($foo);
+//var_dump($undefined); warning but keeps going
+
+/**
+ * 
+ * multi line 
+ * comment 
+ * 
+ */
+
+ 
